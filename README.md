55e2b7e658f7d859a62f9ca5d6826173da22df5f
kli23 <kli23@stuy.edu>
2021-12-07 02:03:41 -0500

    final copy

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # work17
+Compile with make
+Create file, shared memory, and semaphore with ./control create.
+Write to file with ./write
+When prompted with next line?, type your next line the press ctrl + d
+Finally, remove memory and semaphore and display the story with ./control remove
+
+## Bugs:
+Will occasionally read extra characters when printing previous line
--- writer.c
+++ writer.c
@@ -14,13 +14,43 @@
 #define KEY 24602
 
 int main() {
-    char s[100];
+    char sentence[500];
+    int semd, shmd, file;
+    int *linesize;
+    char pastsent[500];
+
+    shmd = shmget(KEY, 0, 0);
+    semd = semget(KEY+1, 1, 0); //get access to sem
+    struct sembuf sb;
+    sb.sem_num = 0;
+    sb.sem_flg = SEM_UNDO;
+    sb.sem_op = -1;
+
+    semop(semd, &sb, 1); //attempts to take semaphore
+    linesize = shmat(shmd,0,0); //attaches segment to linesize variable
+
+    struct stat st;
+    file = open("telephone.txt", O_RDONLY); //checks if file is empty
+    stat("telephone.txt", &st);
+    if (st.st_size) //if not empty 
+    {
+        lseek(file, -*linesize, SEEK_END); //set the position in the file to however many bytes from the end of the file
+    }
+    read(file, pastsent, *linesize);
+    printf("Previous line: %s\n", pastsent);
+
+
+    printf("Next line? "); //prompts user, appends next line to file
+    fgets(sentence, 500,stdin);
+    file = open("telephone.txt", O_WRONLY | O_APPEND);
+    write(file, sentence, strlen(sentence));
+    *linesize = strlen(sentence);
+    shmdt(linesize); //assign linesize and detach segment from variable
 
 
 
 
-    printf("Next line? ");
-    fgets(s,100,stdin);
-
+    sb.sem_op = 1; // set operation to up
+    semop(semd, &sb, 1);
     return 0;
 }
